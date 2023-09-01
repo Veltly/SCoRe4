f2e222ef7ea8b5a5855140f70784bd52d063dee0
Christoph Gruener <mrgreen@fstph.at>
2023-09-01 12:32:29 +0200

    Filestructure created

---
+++ SurfaceGenerator/include/SGenerator.hh
@@ -0,0 +1,21 @@
+//
+//
+//
+//
+//	author: C.Gruener
+//
+//
+
+#include "G4LogicalVolume.hh"
+#include "SAssembler.hh"
+#include "SCalculator.hh"
+#include "SDescriber.hh"
+
+namespace SGenerator {
+
+class SurfaceGenerator {
+
+private:
+};
+
+} // namespace SGenerator
