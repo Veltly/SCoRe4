121f8c2ad6b6ed1bf36d4364f5e9be38c54161eb
Christoph Gruener <christoph.gruener@protonmail.com>
2024-06-05 12:48:57 +0200

    implementation of PeriodicPortal nearly finished

--- src/Portal/include/PeriodicPortal.hh
+++ src/Portal/include/PeriodicPortal.hh
@@ -1,5 +1,5 @@
 // Author: C.Gruener
-// Date: 04-06-24
+// Date: 24-06-04
 // File: PeriodicPortal
 
 #ifndef PERIODICPORTAL_HH
@@ -13,22 +13,44 @@
 namespace Surface {
 
 class PeriodicPortal : public VPortal {
+
+  enum class SingleSurface { X_UP, X_DOWN, Y_UP, Y_DOWN, Z_UP, Z_DOWN };
+  enum class PortationType { ENTER, EXIT, PERIODIC };
+
 public:
   PeriodicPortal(G4String name, G4VPhysicalVolume *volume, G4ThreeVector &vec,
                  G4int verbose);
   virtual void DoPortation(const G4Step *step);
+  void SetGrid(int nX, int nY);
+  inline G4int GetMaxNX() const { return fMaxNX; }
+  inline G4int GetMaxNY() const { return fMaxNY; }
+  inline G4int GetCurrentNX() const { return fCurrentNX; }
+  inline G4int GetCurrentNY() const { return fCurrentNY; }
+  inline G4bool IsPortal() const { return fIsPortal; }
+  void SetAsPortal() { fIsPortal = true; }
 
 private:
-  void DoPeriodicPortation();
-  void EnterPortal();
-  void LeavePortal();
-  void GetExitSurface(const G4Step *step);
+  void DoPeriodicPortation(const G4Step *step, SingleSurface);
+  void EnterPortal(const G4Step *step, SingleSurface);
+  void ExitPortal(const G4Step *step, SingleSurface);
+  SingleSurface GetNearestSurface(const G4Step *step) const;
+  PortationType GetPortationType(SingleSurface) const;
+  void DoPeriodicTransform(G4ThreeVector &vec, SingleSurface);
+  void TransformSubworldToPortal(G4ThreeVector &vec);
+  void TransformPortalToSubworld(G4ThreeVector &vec);
+  G4double TransformZBetweenPortals(G4double val);
 
 private:
   PeriodicPortal *fOtherPortal;
   G4VPhysicalVolume *fTrigger;
-  G4int fNX;
-  G4int fNY;
+  G4bool fIsPortal; // True if it is the portal, false if it is the periodic
+                    // subworld
+  G4int fMaxNX;
+  G4int fMaxNY;
+  G4int fCurrentNX;
+  G4int fCurrentNY;
+  // order of Grid such that (fCurrentNX, fCurrentNy) = (0,0)
+  // has the lowest local (x,y) coordinate in the portal
 };
 } // namespace Surface
 #endif // PERIODICPORTAL_HH
--- src/Portal/src/PeriodicPortal.cc
+++ src/Portal/src/PeriodicPortal.cc
@@ -1,15 +1,14 @@
 // Author: C.Gruener
-// Date: 04-06-025
+// Date: 24-06-04
 // File: PeriodicPortal
 
 #include "../include/PeriodicPortal.hh"
 #include "../include/VPortal.hh"
-#include "G4Box.hh"
 #include "G4Step.hh"
-#include <G4ThreeVector.hh>
-#include <G4VPhysicalVolume.hh>
-
-enum class SingleSurface { X_UP, X_DOWN, Y_UP, Y_DOWN, Z_UP, Z_DOWN };
+#include "G4ThreeVector.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4VSolid.hh"
+#include <cstdlib>
 
 Surface::PeriodicPortal::PeriodicPortal(G4String name,
                                         G4VPhysicalVolume *volume,
@@ -19,26 +18,148 @@ Surface::PeriodicPortal::PeriodicPortal(G4String name,
   SetGlobalCoord(vec);
 };
 
-void Surface::PeriodicPortal::DoPortation(const G4Step *step){
-    // check if Portation is possible (change of volume)
-
-    // check if Portation is triggered( postStepPoint in trigger)
-
-    // select Portation Method by checking Grid and side of trigger
-
-    // -->case PeriodicPortation
-    // update Grid
-
-    // -->case LeavePortal
-
+void Surface::PeriodicPortal::DoPortation(const G4Step *step) {
+  // select portation method by checking Grid and side of exit portal
+  SingleSurface surface = GetNearestSurface(step);
+  PortationType portationType = GetPortationType(surface);
+  switch (portationType) {
+  case PortationType::ENTER: {
+    EnterPortal(step, surface);
+    break;
+  }
+  case PortationType::EXIT: {
+    ExitPortal(step, surface);
+    break;
+  }
+  case PortationType::PERIODIC: {
+    DoPeriodicPortation(step, surface);
+    break;
+  }
+  }
 };
 
+void Surface::PeriodicPortal::EnterPortal(const G4Step *step,
+                                          SingleSurface enterSurface) {
+
+  G4ThreeVector position = step->GetPostStepPoint()->GetPosition();
+  position = TransformToLocalCoordinate(position);
+  TransformPortalToSubworld(position);
+  position = fOtherPortal->TransformToGlobalCoordinate(position);
+  UpdatePosition(step, position);
+}
+
+void Surface::PeriodicPortal::ExitPortal(const G4Step *step,
+                                         SingleSurface exitSurface) {
+  G4ThreeVector position = step->GetPostStepPoint()->GetPosition();
+  position = TransformToLocalCoordinate(position);
+  TransformSubworldToPortal(position);
+  position = fOtherPortal->TransformToGlobalCoordinate(position);
+  UpdatePosition(step, position);
+}
+
+void Surface::PeriodicPortal::DoPeriodicPortation(const G4Step *step,
+                                                  SingleSurface exitSurface) {
+  G4ThreeVector position = step->GetPostStepPoint()->GetPosition();
+  DoPeriodicTransform(position, exitSurface);
+  UpdatePosition(step, position);
+}
+
+Surface::PeriodicPortal::PortationType
+Surface::PeriodicPortal::GetPortationType(SingleSurface surface) const {
+  if (fIsPortal) {
+    return PortationType::ENTER;
+  }
+  switch (surface) {
+  case SingleSurface::X_UP:
+  case SingleSurface::X_DOWN:
+  case SingleSurface::Y_UP:
+  case SingleSurface::Y_DOWN:
+    return PortationType::PERIODIC;
+    break;
+  case SingleSurface::Z_UP:
+  case SingleSurface::Z_DOWN:
+    return PortationType::EXIT;
+    break;
+  default:
+    exit(EXIT_FAILURE); // should never happen
+  }
+}
+
 // Function to decide in which direction the particle left the volume
-// use G4ThreeVector result = solid->SurfaceNormal(point);
-void Surface::PeriodicPortal::GetExitSurface(const G4Step *step) {
+Surface::PeriodicPortal::SingleSurface
+Surface::PeriodicPortal::GetNearestSurface(const G4Step *step) const {
   G4VSolid *portalSolid = GetVolume()->GetLogicalVolume()->GetSolid();
   G4ThreeVector result =
       portalSolid->SurfaceNormal(step->GetPostStepPoint()->GetPosition());
-  // switch
-  // case
+  if (result.x() > result.y() and result.x() > result.z()) {
+    return SingleSurface::X_UP;
+  } else if (result.x() < result.y() and result.x() < result.z()) {
+    return SingleSurface::X_DOWN;
+  } else if (result.y() > result.x() and result.y() > result.z()) {
+    return SingleSurface::Y_UP;
+  } else if (result.y() < result.x() and result.y() < result.z()) {
+    return SingleSurface::Y_DOWN;
+  } else if (result.z() > result.x() and result.z() > result.y()) {
+    return SingleSurface::Z_UP;
+  } else if (result.z() < result.x() and result.z() < result.y()) {
+    return SingleSurface::Z_DOWN;
+  } else {
+    exit(EXIT_FAILURE); // is a last check, shoudl never happen
+  }
+}
+
+void Surface::PeriodicPortal::DoPeriodicTransform(G4ThreeVector &vec,
+                                                  SingleSurface surface) {
+  G4VPhysicalVolume *volume = GetVolume();
+  G4ThreeVector pMin, pMax;
+  volume->GetLogicalVolume()->GetSolid()->BoundingLimits(pMin, pMax);
+  G4ThreeVector volumeSize = pMax - pMin;
+  G4ThreeVector oldPosition = vec;
+  switch (surface) {
+  case SingleSurface::X_UP:
+    vec.setX(oldPosition.x() - volumeSize.x());
+    ++fCurrentNX;
+    break;
+  case SingleSurface::X_DOWN:
+    vec.setX(oldPosition.x() + volumeSize.x());
+    --fCurrentNX;
+    break;
+  case SingleSurface::Y_UP:
+    vec.setY(oldPosition.y() - volumeSize.y());
+    ++fCurrentNY;
+    break;
+  case SingleSurface::Y_DOWN:
+    vec.setY(oldPosition.y() + volumeSize.y());
+    --fCurrentNY;
+    break;
+  default:
+    exit(EXIT_FAILURE); // should never happen
+  }
+}
+
+void Surface::PeriodicPortal::TransformSubworldToPortal(G4ThreeVector &vec) {
+  // TO DO: Implementation
+  vec.setZ(TransformZBetweenPortals(vec.z()));
+}
+
+void Surface::PeriodicPortal::TransformPortalToSubworld(G4ThreeVector &vec) {
+  // TO DO: Implementation
+  // fOtherPortal->fCurrentX and Y must be set
+  vec.setZ(TransformZBetweenPortals(vec.z()));
+}
+
+G4double Surface::PeriodicPortal::TransformZBetweenPortals(G4double val) {
+  G4ThreeVector pMin, pMax;
+  GetVolume()->GetLogicalVolume()->GetSolid()->BoundingLimits(pMin, pMax);
+  G4double volumeDistanceZ = pMax.z() - pMin.z();
+  fOtherPortal->GetVolume()->GetLogicalVolume()->GetSolid()->BoundingLimits(
+      pMin, pMax);
+  G4double otherVolumeDistanceZ = pMax.z() - pMin.z();
+  val = val / volumeDistanceZ * otherVolumeDistanceZ;
+  return val;
+}
+
+void Surface::PeriodicPortal::SetGrid(int nX, int nY) {
+  fMaxNX = nX;
+  fMaxNY = nY;
 }
