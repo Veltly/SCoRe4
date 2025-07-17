fe0104d57782501f96c77746323f05dfd20a4a8b
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-07-17 11:05:32 +0200

    Simplified construction of experimental setup and added comments and doxygen basic information.

--- test/simplePortal_test/src/DetectorConstruction.cc
+++ test/simplePortal_test/src/DetectorConstruction.cc
@@ -1,16 +1,13 @@
-// Author C.Gruener
-// Date 24-05-21
-// File DetectorConstruction
+/**
+ * @brief Construct test setup for a simple portal represented by just one volume.
+ * @author C.Gruener
+ * @date 24-07-17
+ */
 
 #include "DetectorConstruction.hh"
 
-#include <G4MultiUnion.hh>
-#include <G4Region.hh>
-#include <G4RotationMatrix.hh>
 #include <G4ThreeVector.hh>
-#include <G4Transform3D.hh>
 #include <G4UserLimits.hh>
-#include <cfloat>
 
 #include "../../../src/Portal/include/PortalStore.hh"
 #include "../../../src/Portal/include/SimplePortal.hh"
@@ -21,201 +18,195 @@
 #include "G4PVPlacement.hh"
 #include "G4RunManager.hh"
 #include "G4SystemOfUnits.hh"
-#include "Portal/include/VPortal.hh"
 
 DetectorConstruction::DetectorConstruction()
-    : G4VUserDetectorConstruction(), fScoringVolume(0) {}
+    : G4VUserDetectorConstruction(), fScoringVolume(nullptr) {}
 
-DetectorConstruction::~DetectorConstruction() {}
+DetectorConstruction::~DetectorConstruction() = default;
 
 G4VPhysicalVolume *DetectorConstruction::Construct() {
   // Get nist material manager
   G4NistManager *nist = G4NistManager::Instance();
 
-  G4bool checkOverlaps = false;
+  G4bool checkOverlaps{false};
 
   //
-  // World
+  // create World
   //
-  G4double world_sizeXY = 1 * m;
-  G4double world_sizeZ = 1 * m;
+  G4double world_sizeXY{1 * m};
+  G4double world_sizeZ{1 * m};
   G4Material *world_mat = nist->FindOrBuildMaterial("G4_AIR");
 
-  G4Box *solidWorld = new G4Box("World",  // its name
+  auto *solidWorld = new G4Box("World",  // its name
                                 0.5 * world_sizeXY, 0.5 * world_sizeXY,
                                 0.5 * world_sizeZ);  // its size
 
-  G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld,  // its solid
+  auto *logicWorld = new G4LogicalVolume(solidWorld,  // its solid
                                                     world_mat,   // its material
                                                     "World");    // its name
 
   G4VPhysicalVolume *physWorld =
-      new G4PVPlacement(0,                // no rotation
+      new G4PVPlacement(nullptr,                // no rotation
                         G4ThreeVector(),  // at (0,0,0)
                         logicWorld,       // its logical volume
                         "World",          // its name
-                        0,                // its mother  volume
+                        nullptr,                // its mother volume
                         false,            // no boolean operation
                         0,                // copy number
                         checkOverlaps);   // overlaps checking
 
-  //  new G4PVPlacement(0,               // no rotation
-  //                    G4ThreeVector(), // at (0,0,0)
-  //                    logicBox,        // its logical volume
-  //                    "TestBox",       // its name
-  //                    logicWorld,      // its mother  volume
-  //                    false,           // no boolean operation
-  //                    0,               // copy number
-  //                    checkOverlaps);  // overlaps checking
 
   //
   // Subworld to port into
   //
   //
 
-  G4double subworld_sizeXY = 1 * cm;
-  G4double subworld_sizeZ = 1 * cm;
+  G4double subworld_sizeXY{1 * cm}; //size of subworld
+  G4double subworld_sizeZ{1 * cm}; //size of subworld
 
-  G4double subworldTrigger_sizeXY = subworld_sizeXY * 1.1;
-  G4double subworldTrigger_sizeZ = subworld_sizeZ * 1.1;
-  G4ThreeVector subworldTriggerPlacement{5 * cm, 5 * cm, 20 * cm};
-  G4Material *subworldTrigger_mat = nist->FindOrBuildMaterial("G4_AIR");
-  G4Box *solidSubworldTrigger =
+  G4double subworldTrigger_sizeXY{subworld_sizeXY * 1.1}; //trigger size
+  G4double subworldTrigger_sizeZ{subworld_sizeZ * 1.1}; //trigger size
+  G4ThreeVector subworldTriggerPlacement{5 * cm, 5 * cm, 20 * cm}; //placement of trigger
+  G4Material *subworldTrigger_mat = nist->FindOrBuildMaterial("G4_AIR"); //material of trigger is not necessary to be G4_AIR
+  auto *solidSubworldTrigger =
       new G4Box("SubworldTrigger",  // its name
                 0.5 * subworldTrigger_sizeXY, 0.5 * subworldTrigger_sizeXY,
                 0.5 * subworldTrigger_sizeZ);  // its size
 
-  G4LogicalVolume *logicSubworldTrigger =
+  auto *logicSubworldTrigger =
       new G4LogicalVolume(solidSubworldTrigger,  // its solid
                           subworldTrigger_mat,   // its material
                           "SubworldTrigger");    // its name
 
   G4VPhysicalVolume *physSubworldTrigger =
-      new G4PVPlacement(0,                         // no rotation
-                        subworldTriggerPlacement,  //
-                        logicSubworldTrigger,      // its logical volume
-                        "SubworldTrigger",         // its name
-                        logicWorld,                // its mother  volume
-                        false,                     // no boolean operation
+      new G4PVPlacement(nullptr,                       //no rotation
+                        subworldTriggerPlacement,      //its location
+                        logicSubworldTrigger,  // its logical volume
+                        "SubworldTrigger",           // its name
+                        logicWorld,            // its mother volume
+                        false,                       // no boolean operation
                         0,                         // copy number
                         checkOverlaps);            // overlaps checking
 
-  G4ThreeVector subworldPlacement{0, 0, 0};
+  G4ThreeVector subworldPlacement{0, 0, 0}; //subworld location
   G4Material *subworld_mat = nist->FindOrBuildMaterial("G4_AIR");
-  G4Box *solidSubworld = new G4Box("Subworld",  // its name
+  auto *solidSubworld = new G4Box("Subworld",  // its name
                                    0.5 * subworld_sizeXY, 0.5 * subworld_sizeXY,
                                    0.5 * subworld_sizeZ);  // its size
 
-  G4LogicalVolume *logicSubworld =
+  auto *logicSubworld =
       new G4LogicalVolume(solidSubworld,  // its solid
                           subworld_mat,   // its material
                           "Subworld");    // its name
 
   G4VPhysicalVolume *physSubworld =
-      new G4PVPlacement(0,                     // no rotation
+      new G4PVPlacement(nullptr,                     // no rotation
                         subworldPlacement,     //
                         logicSubworld,         // its logical volume
                         "Subworld",            // its name
-                        logicSubworldTrigger,  // its mother  volume
+                        logicSubworldTrigger,  // its mother volume
                         false,                 // no boolean operation
                         0,                     // copy number
                         checkOverlaps);        // overlaps checking
 
-  G4double testobject_XY = subworld_sizeXY * 0.2;
-  G4double testobject_Z = subworld_sizeZ * 0.2;
+  //
+  // create testobject and place three times in subworld
+  //
+
+  G4double testobject_XY{subworld_sizeXY * 0.2};
+  G4double testobject_Z{subworld_sizeZ * 0.2};
   G4ThreeVector testobjectPlacement{0, 0, 0};
   G4Material *testobject_mat = nist->FindOrBuildMaterial("G4_Si");
-  G4Box *solidTestobject = new G4Box("Testobject", 0.5 * testobject_XY,
+  auto *solidTestobject = new G4Box("Testobject", 0.5 * testobject_XY,
                                      0.5 * testobject_XY, 0.5 * testobject_Z);
 
-  G4LogicalVolume *logicTestobject =
+  auto *logicTestobject =
       new G4LogicalVolume(solidTestobject, testobject_mat, "Testobject");
 
-  new G4PVPlacement(0, testobjectPlacement, logicTestobject,
+  new G4PVPlacement(nullptr, testobjectPlacement, logicTestobject,
                     "TestobjectInsideMiddle", logicSubworld, false, 0,
                     checkOverlaps);
   G4ThreeVector testobjectPlacementInsideUpper{0, 0, 0};
   testobjectPlacementInsideUpper.setZ((subworld_sizeZ - testobject_Z) / 2.);
-  new G4PVPlacement(0, testobjectPlacementInsideUpper, logicTestobject,
+  new G4PVPlacement(nullptr, testobjectPlacementInsideUpper, logicTestobject,
                     "TestobjectInsideUpper", logicSubworld, false, 0,
                     checkOverlaps);
   G4ThreeVector testobjectPlacementInsideLower{0, 0, 0};
   testobjectPlacementInsideLower.setZ(-(subworld_sizeZ - testobject_Z) / 2.);
-  new G4PVPlacement(0, testobjectPlacementInsideLower, logicTestobject,
+  new G4PVPlacement(nullptr, testobjectPlacementInsideLower, logicTestobject,
                     "TestobjectInsideLower", logicSubworld, false, 0,
                     checkOverlaps);
   //
-  // Portal entrance
-  //
+  // create portal entrance
   //
 
-  G4double portal_sizeXY = subworld_sizeXY;
-  G4double portal_sizeZ = 2 * subworld_sizeZ;
-  G4ThreeVector portalPlacement{0 * cm, 0 * cm, 10 * cm};
+  G4double portal_sizeXY{subworld_sizeXY}; //size portal
+  G4double portal_sizeZ{2 * subworld_sizeZ};//size portal
+  G4ThreeVector portalPlacement{0 * cm, 0 * cm, 10 * cm};//placement portal
   G4Material *portal_mat = nist->FindOrBuildMaterial("G4_AIR");
-  G4Box *solidPortal = new G4Box("Portal", 0.5 * portal_sizeXY,
+  auto *solidPortal = new G4Box("Portal", 0.5 * portal_sizeXY,
                                  0.5 * portal_sizeXY, 0.5 * portal_sizeZ);
-  G4LogicalVolume *logicPortal =
+  auto *logicPortal =
       new G4LogicalVolume(solidPortal, portal_mat, "Portal");
 
   G4VPhysicalVolume *physPortal =
-      new G4PVPlacement(0, portalPlacement, logicPortal, "Portal", logicWorld,
+      new G4PVPlacement(nullptr, portalPlacement, logicPortal, "Portal", logicWorld,
                         false, 0, checkOverlaps);
   //
   // Add both volumes to portal Store
   //
-  //
 
-  G4double testobjectB_XY = portal_sizeXY;
-  G4double testobjectB_Z = portal_sizeZ;
-  G4ThreeVector testobjectBPlacementLower = portalPlacement;
+  G4double testobjectB_XY{portal_sizeXY};
+  G4double testobjectB_Z{portal_sizeZ};
+  G4ThreeVector testobjectBPlacementLower{portalPlacement};
   testobjectBPlacementLower.setZ(testobjectBPlacementLower.z() -
                                  ((testobjectB_Z + portal_sizeZ) / 2.));
-  G4ThreeVector testobjectBPlacementUpper = portalPlacement;
+  G4ThreeVector testobjectBPlacementUpper{portalPlacement};
   testobjectBPlacementUpper.setZ(testobjectBPlacementUpper.z() +
                                  ((testobjectB_Z + portal_sizeZ) / 2.));
   G4Material *testobjectB_mat = nist->FindOrBuildMaterial("G4_Si");
-  G4Box *solidTestobjectB =
+  auto *solidTestobjectB =
       new G4Box("TestobjectOutside", 0.5 * testobjectB_XY, 0.5 * testobjectB_XY,
                 0.5 * testobjectB_Z);
 
-  G4LogicalVolume *logicTestobjectB = new G4LogicalVolume(
+  auto *logicTestobjectB = new G4LogicalVolume(
       solidTestobjectB, testobjectB_mat, "TestobjectOutside");
 
-  new G4PVPlacement(0, testobjectBPlacementLower, logicTestobjectB,
+  new G4PVPlacement(nullptr, testobjectBPlacementLower, logicTestobjectB,
                     "TestobjectOutsideLower", logicWorld, false, 0,
                     checkOverlaps);
 
-  new G4PVPlacement(0, testobjectBPlacementUpper, logicTestobjectB,
+  new G4PVPlacement(nullptr, testobjectBPlacementUpper, logicTestobjectB,
                     "TestobjectOutsideUpper", logicWorld, false, 0,
                     checkOverlaps);
 
-  Surface::PortalStore &portalStore = Surface::Locator::GetPortalStore();
-  Surface::SimplePortal *portalEntrance =
+  Surface::PortalStore &portalStore = Surface::Locator::GetPortalStore(); //get singleton portal store
+  auto *portalEntrance =
       new Surface::SimplePortal("Entrance", physPortal, portalPlacement, 3);
-  Surface::SimplePortal *portalSubworld = new Surface::SimplePortal(
+  auto *portalSubworld = new Surface::SimplePortal(
       "Subworld", physSubworld, subworldTriggerPlacement, 3);
-  // set trigger for portals
-  portalEntrance->SetTrigger(physPortal);
-  portalSubworld->SetTrigger(physSubworldTrigger);
-  // link portals
-  portalEntrance->SetOtherPortal(portalSubworld);
-  portalSubworld->SetOtherPortal(portalEntrance);
-  // add them to the store
-  portalStore.push_back(portalEntrance);
-  portalStore.push_back(portalSubworld);
 
-  // set StepLimit
+  portalEntrance->SetTrigger(physPortal); //set trigger for portation
+  portalSubworld->SetTrigger(physSubworldTrigger); //set trigger for portation
 
-  G4double maxStepsize = 0.1 * mm;
-  G4UserLimits *limit = new G4UserLimits(maxStepsize);
+  portalEntrance->SetOtherPortal(portalSubworld); //link portals
+  portalSubworld->SetOtherPortal(portalEntrance); //link portals
+
+  portalStore.push_back(portalEntrance); //add entrance to portal store
+  portalStore.push_back(portalSubworld); //add subworld to portal store
+
+
+  //
+  // set step limit
+  //
+
+  G4double maxStepsize{0.1 * mm};
+  auto *limit = new G4UserLimits(maxStepsize);
   logicWorld->SetUserLimits(limit);
   logicSubworld->SetUserLimits(limit);
   logicPortal->SetUserLimits(limit);
   logicTestobject->SetUserLimits(limit);
   logicTestobjectB->SetUserLimits(limit);
-  //
-  // always return the physical World
-  //
-  return physWorld;
+
+  return physWorld; //return physical world
 }
