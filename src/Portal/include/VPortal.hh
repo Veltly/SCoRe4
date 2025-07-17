f74d83e12ba35f069fb3909d5749ba6be6ecacaa
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-07-17 11:15:18 +0200

    added comments for doxygen to VPortal.hh

--- src/Portal/include/VPortal.hh
+++ src/Portal/include/VPortal.hh
@@ -1,6 +1,9 @@
-// Copyright [2024] C.Gruener
-// Date: 24-05-24
-// File: VirtualPortal
+/**
+ * @brief Handles the portal interface
+ * @file VPortal.hh
+ * @author C.Gruener
+ * @date 24-05-24
+ */
 
 #ifndef SRC_PORTAL_INCLUDE_VPORTAL_HH_
 #define SRC_PORTAL_INCLUDE_VPORTAL_HH_
@@ -14,25 +17,33 @@
 
 namespace Surface {
 
+/**
+ * @brief Enumerates the different types of portals
+ */
 enum class PortalType { SimplePortal, PeriodicPortal, MultipleSubworld };
 
+/**
+ * @brief The class is the interface for a portal
+ */
 class VPortal {
  public:
-  virtual void DoPortation(G4Step *) = 0;  //
+  virtual void DoPortation(G4Step *) = 0;  /// Does the portation of a tracked particle
 
-  VPortal(const G4String &name, G4VPhysicalVolume *, const PortalType,
-          const G4int verboseLvl);
-  VPortal(const G4String &name, G4VPhysicalVolume *, const PortalType,
-          const G4int verboseLvl, const G4ThreeVector &globalCoord);
+  VPortal(const G4String &name, G4VPhysicalVolume *, PortalType,
+          G4int verboseLvl);
+  VPortal(const G4String &name, G4VPhysicalVolume *, PortalType,
+          G4int verboseLvl, const G4ThreeVector &globalCoord);
+
   // Getter
   inline G4VPhysicalVolume *GetVolume() const { return fVolume; }
   inline G4String GetName() const { return fName; }
   inline PortalType GetPortalType() const { return fPortalType; }
   inline G4VPhysicalVolume *GetTrigger() const { return fTrigger; }
+
   // Setter
-  void SetGlobalCoord(const G4ThreeVector vec);
-  void SetVerbose(const G4int verboseLvl);
-  void SetTrigger(G4VPhysicalVolume *volume);
+  void SetGlobalCoord(G4ThreeVector vec);
+  void SetVerbose(G4int verboseLvl);
+  void SetTrigger(G4VPhysicalVolume *volume); ///set the trigger of a portal (when trigger is entered, the portal is used
 
  protected:
   G4ThreeVector GetLocalCoordSystem() const;
