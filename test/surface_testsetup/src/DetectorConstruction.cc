6049b42654bdd46bdce11d589b289b8317b2782d
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-08-04 10:23:04 +0200

    Added doxygen comments to surface_testsetup and simplified class

--- test/surface_testsetup/src/DetectorConstruction.cc
+++ test/surface_testsetup/src/DetectorConstruction.cc
@@ -1,6 +1,8 @@
-// Copyright [2024] C.Gruener
-// Date 24-05-21
-// File DetectorConstruction
+/**
+ * @brief detector construction for generating a surface
+ * @file DetectorConstruction.cc
+ * @date 2024-05-21
+ */
 
 #include "../../surface_testsetup/include/DetectorConstruction.hh"
 
@@ -22,132 +24,115 @@
 
 DetectorConstruction::DetectorConstruction()
     : G4VUserDetectorConstruction(),
-      fScoringVolume(0),
+      fScoringVolume(nullptr),
       fSurfaceGenerator(Surface::SurfaceGenerator("Surface_Testsetup")) {}
 
-DetectorConstruction::~DetectorConstruction() {}
+DetectorConstruction::~DetectorConstruction() = default;
 
 G4VPhysicalVolume *DetectorConstruction::Construct() {
-  // Get nist material manager
+  //get nist material manager
   G4NistManager *nist = G4NistManager::Instance();
 
   G4bool checkOverlaps = false;
 
-  //
-  // World
-  //
+  //create world
   G4double world_sizeXY = 1 * m;
   G4double world_sizeZ = 1 * m;
   G4Material *world_mat = nist->FindOrBuildMaterial("G4_AIR");
 
-  G4Box *solidWorld = new G4Box("World",  // its name
+  auto *solidWorld = new G4Box("World",  // its name
                                 0.5 * world_sizeXY, 0.5 * world_sizeXY,
                                 0.5 * world_sizeZ);  // its size
 
-  G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld,  // its solid
-                                                    world_mat,   // its material
-                                                    "World");    // its name
+  auto *logicWorld = new G4LogicalVolume(solidWorld,  // its solid
+                                         world_mat, // its material
+                                         "World");     // its name
 
   G4VPhysicalVolume *physWorld =
-      new G4PVPlacement(0,                // no rotation
+      new G4PVPlacement(nullptr,                // no rotation
                         G4ThreeVector(),  // at (0,0,0)
                         logicWorld,       // its logical volume
                         "World",          // its name
-                        0,                // its mother  volume
+                        nullptr,                // its mother volume
                         false,            // no boolean operation
                         0,                // copy number
                         checkOverlaps);   // overlaps checking
 
-  //
-  // Subworld to place patch of Rough surface
-  //
-  //
+  //subworld to place the patch of Rough surface
   G4double subworld_sizeXY = 1 * cm;
   G4double subworld_sizeZ = 1 * cm;
-  // G4ThreeVector subworldPlacement{40 * cm, 40 * cm, 40 * cm};
   G4ThreeVector subworldPlacement{0 * cm, 0 * cm, 0 * cm};
   G4Material *subworld_mat = nist->FindOrBuildMaterial("G4_AIR");
-  G4Box *solidSubworld = new G4Box("Subworld",  // its name
+
+  auto *solidSubworld = new G4Box("Subworld",  // its name
                                    0.5 * subworld_sizeXY, 0.5 * subworld_sizeXY,
                                    0.5 * subworld_sizeZ);  // its size
 
-  G4LogicalVolume *logicSubworld =
+  auto *logicSubworld =
       new G4LogicalVolume(solidSubworld,  // its solid
                           subworld_mat,   // its material
                           "Subworld");    // its name
 
-  new G4PVPlacement(0,                  // no rotation
+  new G4PVPlacement(nullptr,                  // no rotation
                     subworldPlacement,  //
                     logicSubworld,      // its logical volume
                     "Subworld",         // its name
-                    logicWorld,         // its mother  volume
+                    logicWorld,         // its mother volume
                     false,              // no boolean operation
                     0,                  // copy number
                     checkOverlaps);     // overlaps checking
 
-  ///////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////
-  //
-  //
-  // Patch of rough surface
-  //
-  //
 
+  // generation of a patch of rough surface
   G4Material *roughness_mat = nist->FindOrBuildMaterial("G4_Si");
-  //
-  // Basis
-  //
-  //
 
+  // generation of a basis
   G4double basis_sizeXY = 2 * mm;
   G4double basis_sizeZ = 1 * mm;
-  G4Box *solidBasis = new G4Box("Basis", 0.5 * basis_sizeXY, 0.5 * basis_sizeXY,
+  auto *solidBasis = new G4Box("Basis", 0.5 * basis_sizeXY, 0.5 * basis_sizeXY,
                                 0.5 * basis_sizeZ);
 
   G4ThreeVector basisPlacement{0., 0., -basis_sizeZ * 0.5};
   G4RotationMatrix basisRotation{G4RotationMatrix()};
   G4Transform3D basisTransform(basisRotation, basisPlacement);
-  //
-  // Roughness
-  //
-  //
-  // Set options for roughness
-  Surface::Describer &describer = fSurfaceGenerator.GetDescriber();
-  describer.SetNrSpike_X(2);
-  describer.SetNrSpike_Y(2);
-  describer.SetSpikeWidth_X(0.5 * mm);
-  describer.SetSpikeWidth_Y(0.5 * mm);
-  describer.SetMeanHeight(0.5 * mm);
-  describer.SetNLayer(1);
-  describer.SetHeightDeviation(0.1 * mm);
-  describer.SetSpikeform(Surface::Describer::Spikeform::UniformPyramide);
 
+  // generation of roughness using the describer of the surface generator class
+  Surface::Describer &describer = fSurfaceGenerator.GetDescriber();
+  describer.SetSpikeform(Surface::Describer::Spikeform::UniformPyramide); // set the spike-form (there are predefined spike-forms)
+  describer.SetNrSpike_X(2);  // number of spikes in x-direction
+  describer.SetNrSpike_Y(2);  // number of spikes in y-direction
+  describer.SetSpikeWidth_X(0.5 * mm);  // x-width of spike
+  describer.SetSpikeWidth_Y(0.5 * mm);  // y-width of spike
+  describer.SetMeanHeight(0.5 * mm);  // mean height of spike
+  describer.SetNLayer(1); // number of layers used for a spike
+  describer.SetHeightDeviation(0.1 * mm); // set sigma for deviation from mean height
+
+  // generate rough surface
   fSurfaceGenerator.GenerateSurface();
-  G4MultiUnion *solidRoughness =
-      static_cast<G4MultiUnion *>(fSurfaceGenerator.GetSolid());
+  auto *solidRoughness =
+      dynamic_cast<G4MultiUnion *>(fSurfaceGenerator.GetSolid());
 
   solidRoughness->AddNode(*solidBasis, basisTransform);
 
-  //
-  // Voxelize
-  //
-  //
-  Surface::G4Voxelizer_Green &voxelizer =
-      (Surface::G4Voxelizer_Green &)solidRoughness->GetVoxels();
-  voxelizer.SetMaxBoundary(10, 10, 2);
-  voxelizer.Voxelize(solidRoughness);
+ // Voxelize
+  // ------------------------------------------------------
+  auto &voxelizer =
+      (Surface::G4Voxelizer_Green &)solidRoughness->GetVoxels(); // get voxels from roughness (multiunion)
+  voxelizer.SetMaxBoundary(10, 10, 2); // maximal number of boundaries for voxelization
+  voxelizer.Voxelize(solidRoughness); // voxelize
+  // ------------------------------------------------------
 
-  G4LogicalVolume *logicRoughness =
+  //place roughness in the world
+  auto *logicRoughness =
       new G4LogicalVolume(solidRoughness, roughness_mat, "Roughness");
-  new G4PVPlacement(NULL, G4ThreeVector(), logicRoughness, "Roughness",
-                    logicSubworld, 0, 0, checkOverlaps);
+  new G4PVPlacement(nullptr, G4ThreeVector(), logicRoughness, "Roughness",
+                    logicSubworld, false, 0, checkOverlaps);
 
+  // pass the facets from surface generator the general facet store
   fSurfaceGenerator.SetSurfaceTransformation(subworldPlacement);
   Surface::FacetStore &store = Surface::Locator::GetFacetStore();
   store = *fSurfaceGenerator.GetFacetStore();
-  //
-  // always return the physical World
-  //
+
+  //return the physical World
   return physWorld;
 }
