d1054f21de86be7b1e4b8051f0913d61141eccb2
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-08-04 07:40:57 +0200

    Added doxygen comments to MultiportalHelper_test and simplified class

--- test/multiPortalHelper_test/src/DetectorConstruction.cc
+++ test/multiPortalHelper_test/src/DetectorConstruction.cc
@@ -1,12 +1,16 @@
-// Author C.Gruener
-// Date 24-05-21
-// File DetectorConstruction
+/**
+ * @brief Detector construction for testing.
+ * @details Instance a Portal-Subworld setup using the MultiportalHelper class.
+ * Places a multiportal with 200x200 subworld gridpoints and three sample subworlds A, B, C.
+ * Density for subworlds is 0.3, 0.5, 0.2.
+ * @author C.Gruener
+ * @date 24.05.21
+ * @file DetectorConstruction.cc
+ */
 
 #include "DetectorConstruction.hh"
 
 #include "../../../src/Portal/include/MultipleSubworld.hh"
-#include "../../../src/Portal/include/PortalStore.hh"
-#include "../../../src/Service/include/Locator.hh"
 #include "../../../src/Service/include/MultiportalHelper.hh"
 #include "G4Box.hh"
 #include "G4LogicalVolume.hh"
@@ -14,21 +18,13 @@
 #include "G4PVPlacement.hh"
 #include "G4RunManager.hh"
 #include "G4SystemOfUnits.hh"
-#include "Portal/include/SubworldGrid.hh"
-#include "Portal/include/VPortal.hh"
 #include <CLHEP/Geometry/Transform3D.h>
-#include <G4MultiUnion.hh>
-#include <G4Region.hh>
-#include <G4RotationMatrix.hh>
-#include <G4ThreeVector.hh>
-#include <G4Transform3D.hh>
 #include <G4UserLimits.hh>
-#include <cfloat>
 
 DetectorConstruction::DetectorConstruction()
-    : G4VUserDetectorConstruction(), fScoringVolume(0) {}
+    : G4VUserDetectorConstruction(), fScoringVolume(nullptr) {}
 
-DetectorConstruction::~DetectorConstruction() {}
+DetectorConstruction::~DetectorConstruction() = default;
 
 G4VPhysicalVolume *DetectorConstruction::Construct() {
   // Get nist material manager
@@ -36,91 +32,98 @@ G4VPhysicalVolume *DetectorConstruction::Construct() {
 
   G4bool checkOverlaps = false;
 
-  //
-  // World
-  //
+  // define simulation domain "world"
   const G4double world_sizeXY = 1 * m;
   const G4double world_sizeZ = 1 * m;
   G4Material *world_mat = nist->FindOrBuildMaterial("G4_AIR");
 
-  G4Box *solidWorld = new G4Box("World", // its name
+  auto *solidWorld = new G4Box("World", // its name
                                 0.5 * world_sizeXY, 0.5 * world_sizeXY,
                                 0.5 * world_sizeZ); // its size
 
-  G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, // its solid
+  auto *logicWorld = new G4LogicalVolume(solidWorld, // its solid
                                                     world_mat,  // its material
                                                     "World");   // its name
 
   G4VPhysicalVolume *physWorld =
-      new G4PVPlacement(0,               // no rotation
+      new G4PVPlacement(nullptr,               // no rotation
                         G4ThreeVector(), // at (0,0,0)
                         logicWorld,      // its logical volume
                         "World",         // its name
-                        0,               // its mother  volume
+                        nullptr,               // its mother volume
                         false,           // no boolean operation
                         0,               // copy number
                         checkOverlaps);  // overlaps checking
 
+  //placement of Subworlds A, B, C
+  //placement of subworld such that simulated particles cannot reach them from outside
   const G4ThreeVector placementA{5 * cm, 5 * cm, 22 * cm};
   const G4ThreeVector placementB{5 * cm, 5 * cm, 20 * cm};
   const G4ThreeVector placementC{5 * cm, 5 * cm, 18 * cm};
-
   const G4Transform3D trafoA{G4RotationMatrix(), placementA};
   const G4Transform3D trafoB{G4RotationMatrix(), placementB};
   const G4Transform3D trafoC{G4RotationMatrix(), placementC};
 
+  //placement of Portal
   const G4ThreeVector placementPortal{0 * cm, 0 * cm, 10 * cm};
   const G4Transform3D trafoPortal{G4RotationMatrix(), placementPortal};
 
+  //material of subworld
   G4Material *subworldMaterial = nist->FindOrBuildMaterial("G4_Si");
 
-  Surface::MultiportalHelper helper(5);
+  //instantiate the helper class for portal - subworld setup
+  Surface::MultiportalHelper helper("Helper",5);
 
+  //set size of portal
   helper.SetDxPortal(10 * cm);
   helper.SetDyPortal(10 * cm);
   helper.SetDzPortal(0.5 * cm);
 
+  //set size of single subworld
   helper.SetDxSub(0.5 * mm);
   helper.SetDySub(0.5 * mm);
   helper.SetDzSub(0.5 * cm);
 
+  //set the number of subworlds in direction
   helper.SetNxSub(200);
   helper.SetNySub(200);
 
+  //add the placement of subworlds
   helper.AddSubworldPlacement(trafoA);
   helper.AddSubworldPlacement(trafoB);
   helper.AddSubworldPlacement(trafoC);
 
+  //add density of subworlds
   helper.AddSubworldDensity(0.3);
   helper.AddSubworldDensity(0.5);
   helper.AddSubworldDensity(0.2);
 
+  //set necessary information for helper class
   helper.SetMotherVolume(logicWorld);
-
   helper.SetPortalPlacement(trafoPortal);
-
   helper.SetSubworldMaterial(subworldMaterial);
-
   helper.SetNDifferentSubworlds(3);
-
   helper.SetPortalName("Portal");
+
+  //generate portal-subworld setup
   helper.Generate();
 
+  //get subworlds to set StepLimit
   Surface::MultipleSubworld *subA = helper.GetSubworld(0);
   Surface::MultipleSubworld *subB = helper.GetSubworld(1);
   Surface::MultipleSubworld *subC = helper.GetSubworld(2);
-  //  set StepLimit
 
   G4double maxStepsize = 100 * mm;
   G4double maxStepsize_subworld = 100. * mm;
-  G4UserLimits *limit = new G4UserLimits(maxStepsize);
-  G4UserLimits *limit_subworld = new G4UserLimits(maxStepsize_subworld);
+
+  auto *limit = new G4UserLimits(maxStepsize);
+  auto *limit_subworld = new G4UserLimits(maxStepsize_subworld);
   logicWorld->SetUserLimits(limit);
+
   subA->GetVolume()->GetLogicalVolume()->SetUserLimits(limit_subworld);
   subB->GetVolume()->GetLogicalVolume()->SetUserLimits(limit_subworld);
   subC->GetVolume()->GetLogicalVolume()->SetUserLimits(limit_subworld);
-  //
+
   //   always return the physical World
-  //
   return physWorld;
 }
