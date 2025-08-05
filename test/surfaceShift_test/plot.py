6f30e3ff3a11fe72bb0f6b3b8d69a7e30fc4870d
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-08-05 10:49:25 +0200

    plot.py: uses now pyplot.step(...) and not pyplot.plot(...) to plot a histogram

--- test/surfaceShift_test/plot.py
+++ test/surfaceShift_test/plot.py
@@ -1,12 +1,14 @@
-#! /bin/python3
+#./G4Surface/venv/bin/python
+
 ## plot generated 1D histogram from surfaceShift simulation
+
+import pandas as pd
+from matplotlib import pyplot as plt
 def main():
-    import pandas as pd
-    from matplotlib import pyplot as plt
-    df = pd.read_csv("depth_h1_depth.csv", header=6)
+    df = pd.read_csv("./depth_h1_depth.csv", header=6)
     data = df['entries'].values
     x = [i for i in range(0, len(df))]
-    plt.plot(x, data, marker='.')
+    plt.step(x, data)
     plt.show()
 
 if __name__ == "__main__":
