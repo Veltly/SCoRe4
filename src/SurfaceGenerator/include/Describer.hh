5223d4052ea59e62a11f9a078c264e59527714af
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-08-25 06:47:20 +0200

    Create DescriberParameter struct to gather all parameters.

--- src/SurfaceGenerator/include/Describer.hh
+++ src/SurfaceGenerator/include/Describer.hh
@@ -1,76 +1,87 @@
-// Copyright [2024] C.Gruener
-// Date: 23-06-01
-// File: Describer to hold all information needed to build a rough surface
-// object
-
-#ifndef SRC_SURFACEGENERATOR_INCLUDE_DESCRIBER_HH_
-#define SRC_SURFACEGENERATOR_INCLUDE_DESCRIBER_HH_
+/**
+ * @brief definition of class Describer
+ * @file Describer.hh
+ * @date 2023-06-01
+ * @author C.Gruener
+ */
+#ifndef SRC_SURFACE_GENERATOR_INCLUDE_DESCRIBER_HH_
+#define SRC_SURFACE_GENERATOR_INCLUDE_DESCRIBER_HH_
 
 #include <vector>
-
 #include "../../Service/include/Logger.hh"
 #include "G4Transform3D.hh"
 #include "SurfaceGenerator/include/RectangleDivider.hh"
 #include "SurfaceGenerator/include/Storage.hh"
+
 using Rectangle = Surface::RectangleDivider::Rectangle;
 
 namespace Surface {
 class DescriberMessenger;
-
+/**
+ * @brief The Describer class is used to define a rough surface based on parameters.
+ * These parameters are then translated to G4Solid descriptions and their position.
+ * @details The
+ */
 class Describer {
  public:
-  enum class Spikeform { StandardPyramide, UniformPyramide, Bump, Peak };
+  enum class SpikeShape { StandardPyramid, UniformPyramid, Bump, Peak };
+
+  struct DescriberParameters {
+    G4double spikeWidth_X{1};
+    G4double spikeWidth_Y{1};
+    G4int nSpike_X{1};
+    G4int nSpike_Y{1};
+    G4double meanHeight{1};
+    G4double heightDeviation{1};
+    G4int nLayer{1};
+    Describer::SpikeShape spikeShape{Describer::SpikeShape::StandardPyramid};
+  };
 
  public:
   Describer() noexcept;
+  explicit Describer(const DescriberParameters &params) noexcept;
   void Generate();
 
-  void SetSpikeWidth_X(G4double);
-  void SetSpikeWidth_Y(G4double);
-  void SetNrSpike_X(G4int);
-  void SetNrSpike_Y(G4int);
-  void SetMeanHeight(G4double);
-  void SetHeightDeviation(G4double);
-  void SetSpikeform(Spikeform);
-  void SetNLayer(G4int);
+  //Setter
+  void SetParameters(const DescriberParameters &params){fParams = params;}
+  void SetSpikeWidth_X(G4double width){fParams.spikeWidth_X = width;}
+  void SetSpikeWidth_Y(G4double width){fParams.spikeWidth_Y = width;}
+  void SetNrSpike_X(G4int n){fParams.nSpike_X = n;}
+  void SetNrSpike_Y(G4int n){fParams.nSpike_Y = n;}
+  void SetMeanHeight(G4double height){fParams.meanHeight = height;}
+  void SetHeightDeviation(G4double deviation){fParams.heightDeviation = deviation;}
+  void SetSpikeform(SpikeShape shape){fParams.spikeShape = shape;}
+  void SetNLayer(G4int n){fParams.nLayer = n;}
+
+  //Getter
   G4String GetInfoDescription() const;
   std::vector<SolidDescription> GetSolidDescription() const;
-  inline G4double GetSurfaceWidth_X() const {
-    return fSpikeWidth_X * fNSpike_X;
-  }
-  inline G4double GetSurfaceWidth_Y() const {
-    return fSpikeWidth_Y * fNSpike_Y;
-  }
-  inline G4double GetSpikeWidth_X() const { return fSpikeWidth_X; }
-  inline G4double GetSpikeWidth_Y() const { return fSpikeWidth_Y; }
-  inline G4int GetSpikeNr_X() const { return fNSpike_X; }
-  inline G4int GetSpikeNr_Y() const { return fNSpike_Y; }
-  inline G4double GetMeanHeight() const { return fMeanHeight; }
-  inline G4double GetHeightDeviation() const { return fHeightDeviation; }
-  inline G4int GetNLayer() const { return fNLayer; }
+  G4double GetSurfaceWidth_X() const { return fParams.spikeWidth_X * fParams.nSpike_X;}
+  G4double GetSurfaceWidth_Y() const { return fParams.spikeWidth_Y * fParams.nSpike_Y;}
+  G4double GetSpikeWidth_X() const { return fParams.spikeWidth_X; }
+  G4double GetSpikeWidth_Y() const { return fParams.spikeWidth_Y; }
+  G4int GetSpikeNr_X() const { return fParams.nSpike_X; }
+  G4int GetSpikeNr_Y() const { return fParams.nSpike_Y; }
+  G4double GetMeanHeight() const { return fParams.meanHeight; }
+  G4double GetHeightDeviation() const { return fParams.heightDeviation; }
+  G4int GetNLayer() const { return fParams.nLayer; }
+  SpikeShape GetSpikeShape() const {return fParams.spikeShape; }
 
  private:
   std::vector<SolidDescription> GetSpikeDescription(const Rectangle &);
-  std::vector<SolidDescription> GetStandardPyramid(const Rectangle &);
-  std::vector<SolidDescription> GetUniformPyramid(const Rectangle &);
-  std::vector<SolidDescription> GetBump(const Rectangle &);
-  std::vector<SolidDescription> GetPeak(const Rectangle &);
-  G4Transform3D GetTransformation(const Rectangle &);
-  Surface::RectangleDivider GetRectangle();
+  std::vector<SolidDescription> GetStandardPyramid(const Rectangle &) const;
+  std::vector<SolidDescription> GetUniformPyramid(const Rectangle &) const;
+  std::vector<SolidDescription> GetBump(const Rectangle &) const;
+  std::vector<SolidDescription> GetPeak(const Rectangle &) const;
+  static G4Transform3D GetTransformation(const Rectangle &);
+  Surface::RectangleDivider GetRectangle() const;
   void AppendDescriptionAtPosition(std::vector<SolidDescription> &,
                                    const G4Transform3D &);
 
-  Surface::DescriberMessenger *fMessenger;
-  Spikeform fOptionSpikeform{Spikeform::StandardPyramide};
-  G4double fSpikeWidth_X{1};
-  G4double fSpikeWidth_Y{1};
-  G4int fNSpike_X{1};
-  G4int fNSpike_Y{1};
-  G4double fMeanHeight{1};
-  G4double fHeightDeviation{1};
-  G4int fNLayer{1};
+  DescriberParameters fParams;
   std::vector<SolidDescription> fDescription;
+  Surface::DescriberMessenger *fMessenger;
   Surface::Logger fLogger;
 };
-}  // namespace Surface
-#endif  // SRC_SURFACEGENERATOR_INCLUDE_DESCRIBER_HH_
+}
+#endif
--- src/SurfaceGenerator/src/Describer.cc
+++ src/SurfaceGenerator/src/Describer.cc
@@ -1,12 +1,13 @@
-// Copyright [2024] C.Gruener
-// Date: 23-06-01
-// File:
+/**
+ * @brief Implementation of Describer class
+ * @file Describer.cc
+ * @author C.Gruener
+ * @date 2023-06-01
+ */
 
 #include "SurfaceGenerator/include/Describer.hh"
-
 #include <sstream>
 #include <vector>
-
 #include "G4RotationMatrix.hh"
 #include "G4ThreeVector.hh"
 #include "G4Transform3D.hh"
@@ -16,12 +17,19 @@
 #include "SurfaceGenerator/include/Spike.hh"
 
 Surface::Describer::Describer() noexcept
-    : fMessenger(new DescriberMessenger(this)), fLogger("Describer") {
+    : Describer(Surface::Describer::DescriberParameters{}){}
+
+Surface::Describer::Describer(
+    const Surface::Describer::DescriberParameters &params) noexcept
+    : fParams(params), fMessenger(new DescriberMessenger(this)), fLogger("Describer") {
   fLogger.WriteInfo("initialized");
 }
-
+/**
+ * @brief Generates the description of the rough surface in terms of G4Solids and transformation
+ * @details generates a vector per
+ */
 void Surface::Describer::Generate() {
-  fLogger.WriteDebugInfo("calling Generate");
+  fLogger.WriteDebugInfo("Generate description of surface");
   auto rectangle = GetRectangle();
   auto RectangleIter = rectangle.GetIterBegin();
   auto RectangleIterEnd = rectangle.GetIterEnd();
@@ -46,14 +54,14 @@ G4Transform3D Surface::Describer::GetTransformation(
 
 std::vector<Surface::SolidDescription> Surface::Describer::GetSpikeDescription(
     const Surface::RectangleDivider::Rectangle &aRectangle) {
-  switch (fOptionSpikeform) {
-    case Spikeform::StandardPyramide:
+  switch (fParams.spikeShape) {
+    case SpikeShape::StandardPyramid:
       return GetStandardPyramid(aRectangle);
-    case Spikeform::UniformPyramide:
+    case SpikeShape::UniformPyramid:
       return GetUniformPyramid(aRectangle);
-    case Spikeform::Bump:
+    case SpikeShape::Bump:
       return GetBump(aRectangle);
-    case Spikeform::Peak:
+    case SpikeShape::Peak:
       return GetPeak(aRectangle);
   }
 }
@@ -74,21 +82,21 @@ void Surface::Describer::AppendDescriptionAtPosition(
 }
 
 std::vector<Surface::SolidDescription> Surface::Describer::GetStandardPyramid(
-    const Surface::RectangleDivider::Rectangle &aRectangle) {
+    const Surface::RectangleDivider::Rectangle &aRectangle) const {
   const G4double Width_X{aRectangle.maxX - aRectangle.minX};
   const G4double Width_Y{aRectangle.maxY - aRectangle.minY};
   Surface::Spike Spike{Surface::Spike::Spikeform::Pyramid, Width_X / 2.,
-                       Width_Y / 2., fMeanHeight, 1};
+                       Width_Y / 2., fParams.meanHeight, 1};
   return Spike.GetSpikeDescription();
 }
 
 std::vector<Surface::SolidDescription> Surface::Describer::GetUniformPyramid(
-    const Surface::RectangleDivider::Rectangle &aRectangle) {
+    const Surface::RectangleDivider::Rectangle &aRectangle) const {
   G4double Width_X{aRectangle.maxX - aRectangle.minX};
   G4double Width_Y{aRectangle.maxY - aRectangle.minY};
-  G4double height = G4RandGauss::shoot(fMeanHeight, fHeightDeviation);
+  G4double height = G4RandGauss::shoot(fParams.meanHeight, fParams.heightDeviation);
   if (height <= 0.) {
-    height = 1e-6;
+    height = 1e-9; //minimum height is 1 nm
   }
   Surface::Spike Spike{Surface::Spike::Spikeform::Pyramid, Width_X / 2.,
                        Width_Y / 2., height, 1};
@@ -96,58 +104,31 @@ std::vector<Surface::SolidDescription> Surface::Describer::GetUniformPyramid(
 }
 
 std::vector<Surface::SolidDescription> Surface::Describer::GetBump(
-    const Surface::RectangleDivider::Rectangle &aRectangle) {
+    const Surface::RectangleDivider::Rectangle &aRectangle) const {
   G4double Width_X{aRectangle.maxX - aRectangle.minX};
   G4double Width_Y{aRectangle.maxY - aRectangle.minY};
   Surface::Spike Spike{Surface::Spike::Spikeform::Bump, Width_X / 2.,
-                       Width_Y / 2., fMeanHeight, fNLayer};
+                       Width_Y / 2., fParams.meanHeight, fParams.nLayer};
   return Spike.GetSpikeDescription();
 }
 
 std::vector<Surface::SolidDescription> Surface::Describer::GetPeak(
-    const Surface::RectangleDivider::Rectangle &aRectangle) {
+    const Surface::RectangleDivider::Rectangle &aRectangle) const {
   G4double Width_X{aRectangle.maxX - aRectangle.minX};
   G4double Width_Y{aRectangle.maxY - aRectangle.minY};
   Surface::Spike Spike{Surface::Spike::Spikeform::Peak, Width_X / 2.,
-                       Width_Y / 2., fMeanHeight, fNLayer};
+                       Width_Y / 2., fParams.meanHeight, fParams.nLayer};
   return Spike.GetSpikeDescription();
 }
 
-Surface::RectangleDivider Surface::Describer::GetRectangle() {
-  G4double SurfaceWidth_X{fSpikeWidth_X * fNSpike_X};
-  G4double SurfaceWidth_Y{fSpikeWidth_Y * fNSpike_Y};
+Surface::RectangleDivider Surface::Describer::GetRectangle() const {
+  G4double SurfaceWidth_X{fParams.spikeWidth_X * fParams.nSpike_X};
+  G4double SurfaceWidth_Y{fParams.spikeWidth_Y * fParams.nSpike_Y};
   Surface::RectangleDivider rectangle{1e-12, -SurfaceWidth_X, SurfaceWidth_X,
                                       -SurfaceWidth_Y, SurfaceWidth_Y};
-  rectangle.SplitRectangleEven(fNSpike_X, fNSpike_Y);
+  rectangle.SplitRectangleEven(fParams.nSpike_X, fParams.nSpike_Y);
   return rectangle;
 }
-
-void Surface::Describer::SetSpikeWidth_X(G4double aWidth) {
-  fSpikeWidth_X = aWidth;
-}
-
-void Surface::Describer::SetSpikeWidth_Y(G4double aWidth) {
-  fSpikeWidth_Y = aWidth;
-}
-
-void Surface::Describer::SetNrSpike_X(G4int aN) { fNSpike_X = aN; }
-
-void Surface::Describer::SetNrSpike_Y(G4int aN) { fNSpike_Y = aN; }
-
-void Surface::Describer::SetMeanHeight(G4double aHeight) {
-  fMeanHeight = aHeight;
-}
-
-void Surface::Describer::SetHeightDeviation(G4double aDeviation) {
-  fHeightDeviation = aDeviation;
-}
-
-void Surface::Describer::SetSpikeform(
-    Surface::Describer::Spikeform aSpikeform) {
-  fOptionSpikeform = aSpikeform;
-}
-
-void Surface::Describer::SetNLayer(G4int aNLayer) { fNLayer = aNLayer; }
 
 std::vector<Surface::SolidDescription> Surface::Describer::GetSolidDescription()
     const {
@@ -158,7 +139,7 @@ G4String Surface::Describer::GetInfoDescription() const {
   std::stringstream stream;
   for (auto &description : fDescription) {
     G4String VolumeType;
-    switch (description.Volumetype) {
+    switch (description.VolumeType) {
       case Surface::SolidDescription::Solid::Box:
         VolumeType = "Box";
         break;
@@ -166,9 +147,9 @@ G4String Surface::Describer::GetInfoDescription() const {
         VolumeType = "Trd";
         break;
     }
-    stream << "VolumeType: " << VolumeType << "\n";
-    stream << "Volumeparameter: ";
-    for (auto &value : description.Volumeparameter) {
+    stream << "Volume type: " << VolumeType << "\n";
+    stream << "Volume parameter: ";
+    for (auto &value : description.VolumeParameter) {
       stream << value << ", ";
     }
     stream << "\n";
