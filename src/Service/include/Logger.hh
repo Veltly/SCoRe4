8b852108ef90bf0f40207c1a073250d7fcc9bc1a
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-09-16 09:02:47 +0200

    Added enum class VerboseLevel, added logging of output from a passed function, added logging of vectors to all logging levels, adapted constructor to use VerboseLevel or G4int as arguments.

--- src/Service/include/Logger.hh
+++ src/Service/include/Logger.hh
@@ -10,61 +10,97 @@
 
 #include "G4String.hh"
 #include "G4ThreeVector.hh"
+#include <functional>
 
 namespace Surface {
+/**
+ * @brief Definition of verbose level for logger class
+ */
+enum class VerboseLevel : G4int {
+  Error = 0,
+  Warning = 1,
+  Info = 2,
+  DetailInfo = 3,
+  DebugInfo = 4,
+  Default = 2
+};
+
+/**
+ * @brief comparison operator for VerboseLevel
+ * @param lhs
+ * @param rhs
+ * @return bool
+ */
+constexpr bool operator<=(VerboseLevel lhs,VerboseLevel rhs) {
+  return static_cast<G4int>(lhs) <= static_cast<G4int>(rhs);
+}
+
 /// @brief The class handles generalized logging and the logging level
 class Logger {
  public:
 /// @brief constructor of Logger
 /// @param aLoggerName Name of logger
 /// @param aVerboseLvl logging level of logger
-  explicit Logger(G4String aLoggerName, G4int aVerboseLvl = fgDefaultVerboseLvl);
+  explicit Logger(G4String aLoggerName, VerboseLevel aVerboseLvl = VerboseLevel::Default);
+  explicit Logger(G4String aLoggerName, G4int aVerboseLvl);
 
+ private:
+  static VerboseLevel ToVerboseLvl(G4int aIntVerboseLvl) ;
+
+ public:
   void WriteInfo(const G4String &) const;
   void WriteInfo(const G4String &&) const;
+  void WriteInfo(const G4String &, const G4ThreeVector &) const;
+  void WriteInfo(const G4String &&, const G4ThreeVector &) const;
   void WriteInfo(const std::stringstream &&) const;
-  G4bool WriteInfo() const;
+  void WriteInfo(const std::function<std::string()>& string) const;
 
   void WriteDetailInfo(const G4String &) const;
   void WriteDetailInfo(const G4String &&) const;
+  void WriteDetailInfo(const G4String &, const G4ThreeVector &) const;
+  void WriteDetailInfo(const G4String &&, const G4ThreeVector &) const;
   void WriteDetailInfo(const std::stringstream &&) const;
-  G4bool WriteDetailInfo() const;
+  void WriteDetailInfo(const std::function<std::string()>& string) const;
 
   void WriteWarning(const G4String &) const;
   void WriteWarning(const G4String &&) const;
+  void WriteWarning(const G4String &, const G4ThreeVector &) const;
+  void WriteWarning(const G4String &&, const G4ThreeVector &) const;
   void WriteWarning(const std::stringstream &&) const;
-  G4bool WriteWarning() const;
+  void WriteWarning(const std::function<std::string()>& string) const;
 
   void WriteError(const G4String &) const;
   void WriteError(const G4String &&) const;
+  void WriteError(const G4String &, const G4ThreeVector &) const;
+  void WriteError(const G4String &&, const G4ThreeVector &) const;
   void WriteError(const std::stringstream &&) const;
-  G4bool WriteError() const;
+  void WriteError(const std::function<std::string()>& string) const;
 
   void WriteDebugInfo(const G4String &) const;
   void WriteDebugInfo(const G4String &&) const;
   void WriteDebugInfo(const G4String &, const G4ThreeVector &) const;
   void WriteDebugInfo(const G4String &&, const G4ThreeVector &) const;
   void WriteDebugInfo(const std::stringstream &&) const;
-  G4bool WriteDebugInfo() const;
+  void WriteDebugInfo(const std::function<std::string()>& string) const;
 
   // Setter
-  void SetVerboseLvl(G4int aVerboseLvl);
+  void SetVerboseLvl(VerboseLevel aVerboseLvl);
+  void SetVerboseLvl(G4int aIntVerboseLvl);
 
   // Getter
-  inline G4int GetVerboseLvl() const { return fVerboseLvl; }
-  inline const G4String &GetLoggerName() { return fLoggerName; }
-  static G4int GetDefaultVerboseLvl() {return fgDefaultVerboseLvl;}
+  inline VerboseLevel GetVerboseLvl() const { return fVerboseLvl; }
+  inline const G4String &GetLoggerName() const { return fLoggerName; }
+  static VerboseLevel GetDefaultVerboseLvl() {return VerboseLevel::Default;}
+
+  inline G4bool IsErrorLvl() const {return fVerboseLvl <= VerboseLevel::Error;}
+  inline G4bool IsWarningLvl() const {return fVerboseLvl <= VerboseLevel::Warning;}
+  inline G4bool IsInfoLvl() const {return fVerboseLvl <= VerboseLevel::Info;}
+  inline G4bool IsDetailInfoLvl() const {return fVerboseLvl <= VerboseLevel::DetailInfo;}
+  inline G4bool IsDebugInfoLvl() const {return fVerboseLvl <= VerboseLevel::DebugInfo;}
 
  private:
   G4String fLoggerName; /// logger name of instance
-  G4int fVerboseLvl; /// set verbose level of instance
-  G4int fVerboseError{0}; /// verbose level of error
-  G4int fVerboseWarning{0}; /// verbose level of warning
-  G4int fVerboseInfo{1}; /// verbose level of info
-  G4int fVerboseDetailInfo{2}; /// verbose level of detailed info
-  G4int fVerboseDebugInfo{3}; /// verbose level of debug info
-
-  static const G4int fgDefaultVerboseLvl{4};
+  VerboseLevel fVerboseLvl; /// set verbose level of instance
 };
 }  // namespace Surface
 #endif  // SRC_SERVICE_INCLUDE_LOGGER_HH
--- src/Service/src/Logger.cc
+++ src/Service/src/Logger.cc
@@ -11,11 +11,38 @@
 #include <G4ios.hh>
 #include <utility>
 
-Surface::Logger::Logger(G4String aLoggerName, const G4int aVerboseLvl)
+Surface::Logger::Logger(G4String aLoggerName, const VerboseLevel aVerboseLvl)
     : fLoggerName(std::move(aLoggerName)), fVerboseLvl(aVerboseLvl) {}
 
+Surface::Logger::Logger(G4String aLoggerName, G4int aVerboseLvl)
+    : Logger(std::move(aLoggerName), ToVerboseLvl(aVerboseLvl)){}
+
+Surface::VerboseLevel Surface::Logger::ToVerboseLvl(
+    G4int aIntVerboseLvl) {
+  switch (aIntVerboseLvl){
+    case 0: return Surface::VerboseLevel::Error;
+    case 1: return Surface::VerboseLevel::Warning;
+    case 2: return Surface::VerboseLevel::Info;
+    case 3: return Surface::VerboseLevel::DetailInfo;
+    case 4: return Surface::VerboseLevel::DebugInfo;
+    default: return Surface::VerboseLevel::Default;
+  }
+}
+
+void Surface::Logger::SetVerboseLvl(const VerboseLevel aVerboseLvl) {
+  fVerboseLvl = aVerboseLvl;
+}
+
+void Surface::Logger::SetVerboseLvl(G4int aIntVerboseLvl){
+  SetVerboseLvl(ToVerboseLvl(aIntVerboseLvl));
+}
+
+// ----------------------------------------------
+// -------------------- INFO --------------------
+// ----------------------------------------------
+
 void Surface::Logger::WriteInfo(const G4String &aMsg) const {
-  if (fVerboseLvl > fVerboseInfo) {
+  if (IsInfoLvl()) {
     G4cout << " ----Info---------> " << fLoggerName << ": " << aMsg << G4endl;
   }
 }
@@ -24,31 +51,72 @@ void Surface::Logger::WriteInfo(const G4String &&aMsg) const {
   WriteInfo(aMsg);
 }
 
+void Surface::Logger::WriteInfo(const G4String &aMsg,
+                                      const G4ThreeVector &aVec) const {
+  if (IsInfoLvl()) {
+    G4cout << " ----DebugInfo----> " << fLoggerName << ": " << aMsg
+           << " X: " << aVec.x() << " Y: " << aVec.y() << " Z: " << aVec.z()
+           << G4endl;
+  }
+}
+
+void Surface::Logger::WriteInfo(const G4String &&aMsg, const G4ThreeVector &aVec) const {
+  WriteInfo(aMsg,aVec);
+}
+
 void Surface::Logger::WriteInfo(const std::stringstream &&aMsg) const {
   WriteInfo(aMsg.str());
 }
 
+void Surface::Logger::WriteInfo(const std::function<std::string()> &string) const {
+  if(IsInfoLvl()){
+    G4cout << string() <<G4endl;
+  }
+}
+
+// ----------------------------------------------
+// ----------------- DETAIL INFO ----------------
+// ----------------------------------------------
+
 void Surface::Logger::WriteDetailInfo(const G4String &aMsg) const {
-  if (fVerboseLvl > fVerboseDetailInfo) {
+  if (IsDetailInfoLvl()) {
     G4cout << " ----Info---------> " << fLoggerName << ": " << aMsg << G4endl;
   }
 }
 
-G4bool Surface::Logger::WriteInfo() const { return fVerboseLvl > fVerboseInfo; }
-
 void Surface::Logger::WriteDetailInfo(const G4String &&aMsg) const {
   WriteDetailInfo(aMsg);
 }
+
+void Surface::Logger::WriteDetailInfo(const G4String &aMsg,
+                                 const G4ThreeVector &aVec) const {
+  if (IsDetailInfoLvl()) {
+    G4cout << " ----DebugInfo----> " << fLoggerName << ": " << aMsg
+           << " X: " << aVec.x() << " Y: " << aVec.y() << " Z: " << aVec.z()
+           << G4endl;
+  }
+}
+
+void Surface::Logger::WriteDetailInfo(const G4String &&aMsg, const G4ThreeVector &aVec) const {
+  WriteDetailInfo(aMsg,aVec);
+}
+
 void Surface::Logger::WriteDetailInfo(const std::stringstream &&aMsg) const {
   WriteDetailInfo(aMsg.str());
 }
 
-G4bool Surface::Logger::WriteDetailInfo() const {
-  return fVerboseLvl > fVerboseDetailInfo;
+void Surface::Logger::WriteDetailInfo(const std::function<std::string()> &string) const {
+  if(IsDetailInfoLvl()){
+    G4cout << string() <<G4endl;
+  }
 }
 
+// ----------------------------------------------
+// ------------------- WARNING ------------------
+// ----------------------------------------------
+
 void Surface::Logger::WriteWarning(const G4String &aMsg) const {
-  if (fVerboseLvl > fVerboseWarning) {
+  if (IsWarningLvl()) {
     G4cout << " ----Warning------> " << fLoggerName << ": " << aMsg << G4endl;
   }
 }
@@ -56,16 +124,37 @@ void Surface::Logger::WriteWarning(const G4String &aMsg) const {
 void Surface::Logger::WriteWarning(const G4String &&aMsg) const {
   WriteWarning(aMsg);
 }
+
+void Surface::Logger::WriteWarning(const G4String &aMsg,
+                                 const G4ThreeVector &aVec) const {
+  if (IsWarningLvl()) {
+    G4cout << " ----DebugInfo----> " << fLoggerName << ": " << aMsg
+           << " X: " << aVec.x() << " Y: " << aVec.y() << " Z: " << aVec.z()
+           << G4endl;
+  }
+}
+
+void Surface::Logger::WriteWarning(const G4String &&aMsg,
+                                 const G4ThreeVector &aVec) const {
+  WriteWarning(aMsg, aVec);
+}
+
 void Surface::Logger::WriteWarning(const std::stringstream &&aMsg) const {
   WriteWarning(aMsg.str());
 }
 
-G4bool Surface::Logger::WriteWarning() const {
-  return fVerboseLvl > fVerboseDetailInfo;
+void Surface::Logger::WriteWarning(const std::function<std::string()> &string) const {
+  if(IsWarningLvl()){
+    G4cout << string() <<G4endl;
+  }
 }
 
+// ----------------------------------------------
+// -------------------- ERROR -------------------
+// ----------------------------------------------
+
 void Surface::Logger::WriteError(const G4String &aMsg) const {
-  if (fVerboseLvl > fVerboseError) {
+  if (IsErrorLvl()) {
     G4cout << " ----Error--------> " << fLoggerName << ": " << aMsg << G4endl;
   }
 }
@@ -74,16 +163,36 @@ void Surface::Logger::WriteError(const G4String &&aMsg) const {
   WriteError(aMsg);
 }
 
+void Surface::Logger::WriteError(const G4String &aMsg,
+                                     const G4ThreeVector &aVec) const {
+  if (IsErrorLvl()) {
+    G4cout << " ----DebugInfo----> " << fLoggerName << ": " << aMsg
+           << " X: " << aVec.x() << " Y: " << aVec.y() << " Z: " << aVec.z()
+           << G4endl;
+  }
+}
+
+void Surface::Logger::WriteError(const G4String &&aMsg,
+                                 const G4ThreeVector &aVec) const {
+  WriteError(aMsg, aVec);
+}
+
 void Surface::Logger::WriteError(const std::stringstream &&aMsg) const {
   WriteError(aMsg.str());
 }
 
-G4bool Surface::Logger::WriteError() const {
-  return fVerboseLvl > fVerboseDetailInfo;
+void Surface::Logger::WriteError(const std::function<std::string()> &string) const {
+  if(IsErrorLvl()){
+    G4cout << string() <<G4endl;
+  }
 }
 
+// ----------------------------------------------
+// ----------------- DEBUG INFO -----------------
+// ----------------------------------------------
+
 void Surface::Logger::WriteDebugInfo(const G4String &aMsg) const {
-  if (fVerboseLvl > fVerboseDebugInfo) {
+  if (IsDebugInfoLvl()) {
     G4cout << " ----DebugInfo----> " << fLoggerName << ": " << aMsg << G4endl;
   }
 }
@@ -92,27 +201,27 @@ void Surface::Logger::WriteDebugInfo(const G4String &&aMsg) const {
   WriteDebugInfo(aMsg);
 }
 
-void Surface::Logger::WriteDebugInfo(const G4String &&aMsg,
-                                     const G4ThreeVector &aVec) const {
-  WriteDebugInfo(aMsg, aVec);
-}
 void Surface::Logger::WriteDebugInfo(const G4String &aMsg,
                                      const G4ThreeVector &aVec) const {
-  if (fVerboseLvl > fVerboseDebugInfo) {
+  if (IsDebugInfoLvl()) {
     G4cout << " ----DebugInfo----> " << fLoggerName << ": " << aMsg
            << " X: " << aVec.x() << " Y: " << aVec.y() << " Z: " << aVec.z()
            << G4endl;
   }
 }
 
+void Surface::Logger::WriteDebugInfo(const G4String &&aMsg,
+                                     const G4ThreeVector &aVec) const {
+  WriteDebugInfo(aMsg, aVec);
+}
+
 void Surface::Logger::WriteDebugInfo(const std::stringstream &&stream) const {
   WriteDebugInfo(stream.str());
 }
 
-G4bool Surface::Logger::WriteDebugInfo() const {
-  return fVerboseLvl > fVerboseDetailInfo;
+void Surface::Logger::WriteDebugInfo(const std::function<std::string()> &string) const {
+  if(IsDebugInfoLvl()){
+    G4cout << string() <<G4endl;
+  }
 }
 
-void Surface::Logger::SetVerboseLvl(const G4int aVerboseLvl) {
-  fVerboseLvl = aVerboseLvl;
-}
