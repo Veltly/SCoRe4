a4c050d17642506be58fdbc2ea43c3ee3cd5c8b8
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-10-04 23:22:12 +0200

    Added file HeightMap.py to generate a height map. Currently only wavelike map possible. Added generation of mesh from HeightMap. Added __repr__ and __str__. Cleaned other functions and added more member variables + @properties.

---
+++ src/ParameterToSurface/HeightMap.py
@@ -0,0 +1,56 @@
+import numpy as np
+from enum import Enum
+import matplotlib.pyplot as plt
+
+class HeightMap:
+    class Direction(Enum):
+        X = 0
+        Y = 1
+
+    def __init__(self, n : tuple[int,int], length : tuple[float,float] ) -> None:
+        self.nx = n[0]
+        self.ny = n[1]
+        self.length_x = length[0]
+        self.length_y = length[1]
+        self.dx = length[0] / self.nx
+        self.dy = length[1] / self.ny
+        self._heightmap = None
+
+    @property
+    def heightmap(self):
+        if self._heightmap is None:
+            raise ValueError("No height map generated!")
+        return self._heightmap
+    def wave(self,frequency : float, amplitude : float, direction : Direction = Direction.X ) -> None:
+        if direction == HeightMap.Direction.X:
+            x = np.linspace(0, self.length_x, self.nx)
+            wave = amplitude * np.sin(2 * np.pi * frequency * x) + amplitude
+            heightmap = wave[np.newaxis, :]
+            heightmap = np.broadcast_to(heightmap, (self.ny, wave.size))
+        elif direction == HeightMap.Direction.Y:
+            y = np.linspace(0, self.length_y, self.ny)
+            wave = amplitude * np.sin(2 * np.pi * frequency * y) + amplitude
+            heightmap = wave[:, np.newaxis]
+            heightmap = np.broadcast_to(heightmap, (wave.size, self.nx))
+        else:
+            raise ValueError("Invalid direction argument!")
+        self._heightmap = np.array(heightmap)
+
+    def plot(self):
+        plt.imshow(self.heightmap,
+           origin='lower',   # so y=0 is at bottom
+           extent=(0., self.length_x, 0., self.length_y),
+           cmap='viridis',   # color map
+           aspect='auto')    # make axes proportional
+        plt.colorbar(label='Height')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Height Map')
+        plt.show()
+    def __str__(self):
+        return str(self.heightmap)
+
+if __name__ == "__main__":
+    heightMap = HeightMap((2000,2000),(1.,1.))
+    heightMap.wave(frequency = 1., amplitude = 10., direction = HeightMap.Direction.X)
+    heightMap.plot()
--- src/ParameterToSurface/Surface.py
+++ src/ParameterToSurface/Surface.py
@@ -1,15 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 from abc import ABC, abstractmethod
-from line_profiler import LineProfiler
 from numba import njit
-
 import numpy as np
 import trimesh
 
-
-
-
+from src.ParameterToSurface.HeightMap import HeightMap
 
 
 @dataclass
@@ -117,14 +113,34 @@ class PyramidVolume(Volume):
         return 4
 
 class Surface:
-    def __init__(self, surface_description: SurfaceDescriptionOptions):
-        self.surface_description = surface_description
-        self.mesh = self.generate_surface_mesh()
+    class Source(Enum):
+        DESCRIPTION = 0
+        HEIGHTMAP = 1
+
+    def __init__(self, **kwargs ) -> None:
         self._mean_height = None
         self._root_mean_square_height = None
         self._skewness = None
         self._kurtosis = None
         self._base_area = None
+        self._dx = None
+        self._dy = None
+        self._length_x = None
+        self._length_y = None
+        self._nx = None
+        self._ny = None
+        self._src = None
+        self._surface_description = None
+        if "description" in kwargs:
+            self._src = "description"
+            surface_description = kwargs.get("description")
+            self.mesh = self.generate_surface_mesh_from_description(surface_description)
+        elif "heightmap" in kwargs:
+            self._src = "heightmap"
+            surface_height = kwargs.get("heightmap")
+            self.mesh = self.generate_surface_mesh_from_height_map(surface_height)
+        else:
+            raise ValueError("Require kwarg `description` or `heightmap`")
 
     @property
     def mean_height(self) -> float:
@@ -154,10 +170,61 @@ class Surface:
             self._base_area = self.calculate_base_area()
         return self._base_area
 
-    def generate_surface_mesh(self):
-        number = self.surface_description.spike_number
-        width = self.surface_description.spike_width
-        height = self.surface_description.spike_mean_height
+    @property
+    def dx(self) -> float:
+        if self._dx is None:
+            raise ValueError("Value dx not set!")
+        return self._dx
+
+    @property
+    def dy(self) -> float:
+        if self._dy is None:
+            raise ValueError("Value dy not set!")
+        return self._dy
+
+    @property
+    def length_x(self) -> float:
+        if self._length_x is None:
+            raise ValueError("Value length_x not set!")
+        return self._length_x
+
+    @property
+    def length_y(self) -> float:
+        if self._length_y is None:
+            raise ValueError("Value length_y not set!")
+        return self._length_y
+
+    @property
+    def nx(self) -> int:
+        if self._nx is None:
+            raise ValueError("Value nx not set!")
+        return self._nx
+
+    @property
+    def ny(self) -> int:
+        if self._ny is None:
+            raise ValueError("Value ny not set!")
+        return self._ny
+
+    @property
+    def source(self) -> str:
+        if self._src is None:
+            raise ValueError("Value source not set!")
+        return self._src
+
+    @property
+    def surface_description(self) -> SurfaceDescriptionOptions:
+        if self._surface_description is None:
+            raise ValueError("Value surface_description not set!")
+        return self._surface_description
+
+    def generate_surface_mesh_from_description(self, surface_description) -> trimesh.Trimesh:
+        number = surface_description.spike_number
+        width = surface_description.spike_width
+        self._length_x = number * width
+        self._length_y = number * width
+        self._surface_description = surface_description
+        height = surface_description.spike_mean_height
         number_of_vertices = PyramidVolume.number_of_vertices()
         max_width_half = width * number * 0.5
         #generate grid for placing volumes
@@ -177,6 +244,41 @@ class Surface:
         #return mesh
         return trimesh.Trimesh(vertices, faces)
 
+    def generate_surface_mesh_from_height_map(self, surface_height) -> trimesh.Trimesh:
+        nx = surface_height.nx
+        ny = surface_height.ny
+        self._nx = nx
+        self._ny = ny
+        length_x = surface_height.length_x
+        length_y = surface_height.length_y
+        self._length_x = length_x
+        self._length_y = length_y
+        x = np.linspace(-length_x * 0.5, length_x * 0.5, nx)
+        y = np.linspace(-length_y * 0.5, length_y * 0.5, ny)
+        self._dx = length_x / nx
+        self._dy = length_y / ny
+        grid_x, grid_y = np.meshgrid(x, y)
+        heightmap = surface_height.heightmap
+        vertices = np.column_stack([grid_x.ravel(), grid_y.ravel(), heightmap.reshape(-1)])
+
+        faces = np.empty((2 * (nx - 1) * (ny - 1), 3), dtype=int)
+        i, j = np.meshgrid(np.arange(nx - 1), np.arange(ny - 1), indexing='xy')
+        base = i + j * nx
+        base = base.ravel()
+        faces[0::2, :] = np.stack([base, base + 1, base + 1 + nx], axis=1)
+        faces[1::2, :] = np.stack([base, base + 1 + nx, base + nx], axis=1)
+        #faces_test = np.empty((2 * (nx - 1) * (ny - 1), 3), dtype=int)
+        #idx = 0
+        #for j in range(ny - 1):
+        #    for i in range(nx - 1):
+        #        lower_triangle = np.array([i, i + 1, i + 1 + nx]) + j * nx
+        #        upper_triangle = np.array([i, i + 1 + nx, i + nx]) + j * nx
+        #        faces_test[idx] = lower_triangle
+        #        faces_test[idx + 1] = upper_triangle
+        #        idx += 2
+        mesh = trimesh.Trimesh(vertices, faces)
+        return mesh
+
     def height_map(self,nx:int, ny:int):
         x_min, y_min, z_min = self.mesh.bounds[0]
         x_max, y_max, z_max = self.mesh.bounds[1]
@@ -194,12 +296,9 @@ class Surface:
 
     @staticmethod
     def facet_mean_height(v1: np.ndarray, v2:np.ndarray, v3:np.ndarray) -> float:
-        area = np.linalg.norm(np.cross(v2 - v1, v3 - v1)) * 0.5
+        area = 0.5 * np.linalg.norm(np.cross(v2 - v1, v3 - v1))
         integral = area * (v1[2] + v2[2] + v3[2]) / 3.0
         return integral
-
-    def surface_description(self):
-        pass
 
     @staticmethod
     @njit
@@ -319,46 +418,91 @@ class Surface:
         return total
 
     def __str__(self):
-        pass
+        base_info = (
+            f"Surface ({self.source})\n"
+            f"  Length: x={self.length_x:.3f}, y={self.length_y:.3f};\n"
+            f"  Base area: {self.base_area:.3f}\n"
+        )
+
+        if self.source == "description":
+            extra = (
+                f"  Number of Spikes: {self.surface_description.spike_number * self.surface_description.spike_number}\n"
+                f"  Spike  width: {self.surface_description.spike_width:.3f}\n"
+                f"  Spike mean height: {self.surface_description.spike_mean_height:.3f}\n"
+            )
+        elif self.source == "heightmap":
+            extra = (
+                f"  Number of grid points: x={self.nx},  y={self.ny}\n"
+                f"  Resolution: dx={self.dx:.3e},  dy={self.dy:.3e}\n"
+            )
+        else:
+            extra = ()
+
+        calculations = (
+            f"  Sv:  {self.mean_height: .3f}\n"
+            f"  Sq:  {self.root_mean_square_height: .3f}\n"
+            f"  Sku: {self.kurtosis: .3f}\n"
+            f"  Skk: {self.skewness: .3f}\n"
+        )
+        return base_info + extra + calculations
 
     def __repr__(self):
-        pass
+        return (f"<Surface(source={self.source}, "
+                f"length_x={self.length_x}, length_y={self.length_y})>")
+
+    def show(self):
+        self.mesh.show()
 
     def test_calculation(self, samples = 1000, plot = False):
         import surfalize
-        height_map = surface.height_map(samples,samples)
-        dx = description.spike_width * description.spike_number / samples
-        dy = dx
+        height_map = self.height_map(samples,samples)
+        dx = self.length_x / samples
+        dy = self.length_y / samples
         test_surface = surfalize.Surface(height_map, dx, dy)
         test_sv, test_sq, test_ssk, test_sku = test_surface.roughness_parameters(['Sv', 'Sq', 'Ssk','Sku']).values()
         diff_sv = test_sv - self.mean_height
         diff_sq = test_sq - self.root_mean_square_height
         diff_ssk = test_ssk - self.skewness
         diff_sku = test_sku - self.kurtosis
-        print()
-        print(f'-------------------------------------------------------')
-        print(f'Comparison of surface parameters with library surfalize')
-        print(f'Use {samples} x {samples} samples to translate mesh to surfalize representation')
-        print(f'Area in x-y plane: {self.base_area} um^2')
-        print(f'dx = {dx} um, dy = {dy} um')
-        print(f'-------------------------------------------------------')
-        print(f'         Test :   Real   : Difference : Relative Difference [%]')
-        print(f' Sv: {test_sv: .5f} : {self.mean_height: .5f} : {diff_sv: .3e} : {diff_sv / self.mean_height * 100.: .2f}')
-        print(f' Sq: {test_sq: .5f} : {self.root_mean_square_height: .5f} : {diff_sq: .3e} : {diff_sq / self.root_mean_square_height * 100.: .2f}')
-        print(f'Ssk: {test_ssk: .5f} : {self.skewness: .5f} : {diff_ssk: .3e} : {diff_ssk / self.skewness * 100.: .2f}')
-        print(f'Sku: {test_sku: .5f} : {self.kurtosis: .5f} : {diff_sku: .3e} : {diff_sku / self.kurtosis * 100: .2f}')
-        print(f'-------------------------------------------------------')
+        print(
+            f"\n"
+            f"-------------------------------------------------------\n"
+            f"Comparison of surface parameters with library surfalize\n"
+            f"Use {samples} x {samples} samples to translate mesh to surfalize representation\n"
+            f"Area in x-y plane: {self.base_area} um^2\n"
+            f"dx = {dx} um, dy = {dy} um\n"
+            f"-------------------------------------------------------\n"
+            f"         Test :   Real   : Difference : Relative Difference [%]\n"
+            f" Sv: {test_sv: .5f} : {self.mean_height: .5f} : {diff_sv: .3e} : {diff_sv / self.mean_height * 100.: .2f}\n"
+            f" Sq: {test_sq: .5f} : {self.root_mean_square_height: .5f} : {diff_sq: .3e} : {diff_sq / self.root_mean_square_height * 100.: .2f}\n"
+            f"Ssk: {test_ssk: .5f} : {self.skewness: .5f} : {diff_ssk: .3e} : {diff_ssk / self.skewness * 100.: .2f}\n"
+            f"Sku: {test_sku: .5f} : {self.kurtosis: .5f} : {diff_sku: .3e} : {diff_sku / self.kurtosis * 100: .2f}\n"
+            f"-------------------------------------------------------\n")
 
         if plot:
             test_surface.show()
 
 
-if __name__ == "__main__":
+def test_height_map():
+    height_map = HeightMap((3000,3000), (1.,1.))
+    height_map.wave(1.,0.2)
+    surface = Surface(heightmap=height_map)
+    print(surface)
+    surface.test_calculation(100,False)
+
+
+def test_surface_description():
     description = SurfaceDescriptionOptions()
     description.spike_width = 1.
     description.spike_mean_height = 9.3
     description.spike_layers = 1
     description.spike_number = 100
     description.spike_form = SpikeForm.STANDARD
-    surface = Surface(description)
+    surface = Surface(description=description)
+    print(surface)
     surface.test_calculation(1000,False)
+
+
+if __name__ == "__main__":
+    #test_surface_description()
+    test_height_map()
