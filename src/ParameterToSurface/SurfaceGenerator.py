de151c613d62b21a824321f386f8ddd53c7ab295
Christoph Gruener <christoph.gruener@tuwien.ac.at>
2025-10-14 09:58:02 +0200

    Added control flow of program.

--- src/ParameterToSurface/SurfaceGenerator.py
+++ src/ParameterToSurface/SurfaceGenerator.py
@@ -4,6 +4,10 @@ import yaml
 from pathlib import Path
 import sys
 
+from src.ParameterToSurface.GDMLWriter import GDMLWriter
+from src.ParameterToSurface.HeightMap import HeightMap, HeightMapParameters
+from src.ParameterToSurface.Surface import Surface
+
 # --- Config values with type and default argument ---
 #define all config values here. Is later used by argparse and class Config.
 surface_params = {
@@ -21,8 +25,8 @@ surface_params = {
     "max_height": [float, 10.],
     "min_height": [float, 0.],
     "seed": [int, None],
-    "export_path": [str, "surface_default"],
-    "plot": [bool, False],
+    "export_name": [str, "default"],
+    "solid_name": [str, "solid"]
 }
 
 class Config:
@@ -101,14 +105,47 @@ class Config:
 
 # control flow of program after configuration is generated
 def exec_program(config: Config, control) -> None:
+    if control["config_only"]:
+        return
 
-    #generate height map
-    #plot height map + stop
-    #generate surface
-    #plot surface + stop
-    #plot info + stop
-    #generate gdml
-    pass
+    heightmap = None
+    if control["heightmap"] or control["parameters"] or control["surface"] or control["gdml"]:
+        # --- Generate height map
+        heightmap = HeightMap(n=(config["grid_nx"], config["grid_ny"]),length=(config["length_dx"], config["length_dy"]))
+
+        heightmap_params = HeightMapParameters()
+        heightmap_params.point_cluster_rounds = config["point_cluster_rounds"]
+        heightmap_params.point_cluster_diameter = config["point_cluster_diameter"]
+        heightmap_params.length_cluster_rounds = config["length_cluster_rounds"]
+        heightmap_params.length_cluster_length = config["length_cluster_length"]
+        heightmap_params.length_cluster_width = config["length_cluster_width"]
+        heightmap_params.even_out_rounds = config["even_out_rounds"]
+        heightmap_params.edge_height = config["edge_height"]
+        heightmap_params.max_height = config["max_height"]
+        heightmap_params.min_height = config["min_height"]
+        heightmap_params.seed = config["seed"]
+
+        heightmap.random_complex_all(heightmap_params)
+
+    if control["heightmap"]:
+        plot_heightmap_export_path = str(config["export_path"]) + "_heightmap"
+        show_plot = ~control["silent"]
+        heightmap.plot(plot_heightmap_export_path, show=show_plot)
+
+    surface = None
+    if control["surface"] or control["parameters"] or control["gdml"]:
+        surface = Surface(heightmap=heightmap)
+
+        if control["surface"] and not control["silent"]:
+            surface.show()
+
+        if control["parameters"]:
+            print(surface)
+
+    if control["gdml"]:
+        gdml_filename = config["export_name"]
+        solid_name = config["solid_name"]
+        GDMLWriter(surface.mesh.vertices, surface.mesh.faces, solid_name, gdml_filename)
 
 
 def main():
@@ -121,6 +158,19 @@ def main():
     config_control.add_argument("--config", help="Path to configuration file", type=str)
     config_control.add_argument("--write", help="Write configuration to file", type=str)
     config_control.add_argument("--write-default", help="Write config to default config file", action="store_true")
+
+    # --- Program control flow ---
+    control_group = parser.add_argument_group("Program control")
+    control_params = {
+        "config_only": "Updates only configuration",
+        "heightmap": "Plot height map",
+        "parameters": "Calculate parameters",
+        "surface": "Plot surface",
+        "gdml": "Generate GDML file",
+        "silent": "Generate no graphical output"
+    }
+    for name, description in control_params.items():
+        control_group.add_argument(f"--{name.replace('_','-')}", help=f"{description}", action="store_true", default=False)
 
     # --- Surface control options ---
     surface_group = parser.add_argument_group("Surface control options",
@@ -162,7 +212,8 @@ def main():
         print("Writing config to default config file")
 
     # --- Execute program ---
-    #exec_program(config, control)
+    control = {key: getattr(args, key) for key in control_params if getattr(args, key)}
+    exec_program(config, control)
 
 if __name__ == "__main__":
     main()
